34295f52918b6106d9e46a6325a1b5ac97678807
Jxnuyang <88770945+Jxnuyang@users.noreply.github.com>
2022-03-19 00:03:38 +0800

    Update utils.go

--- TimeBank/utils/utils.go
+++ TimeBank/utils/utils.go
@@ -5,18 +5,21 @@ import (
 	"errors"
 	"fmt"
 	"math/rand"
+	"strings"
+	"time"
 
 	"github.com/hyperledger/fabric-chaincode-go/shim"
 )
 
-var letters = []rune("1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-
-func RandSeq(n int) string {
-	b := make([]rune, n)
-	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
+func RandSqe(lenNum int) string {
+	var chars = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}
+	str := strings.Builder{}
+	length := len(chars)
+	rand.Seed(time.Now().UnixNano()) //重新播种，否则值不会变
+	for i := 0; i < lenNum; i++ {
+		str.WriteString(chars[rand.Intn(length)])
 	}
-	return string(b)
+	return str.String()
 }
 
 func WriteLedger(obj interface{}, stub shim.ChaincodeStubInterface, objectType string, args []string) error {
